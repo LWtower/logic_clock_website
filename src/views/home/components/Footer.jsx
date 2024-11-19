ef1ec2bee6cc5ccf32f216c67338f18e7682559c
unknown <1054164019@qq.com>
2024-11-19 20:19:08 +0800

    footer completed

--- src/views/home/components/Footer.jsx
+++ src/views/home/components/Footer.jsx
@@ -3,7 +3,12 @@ export default function Content() {
   return (
     <>
       <div className="home-footer">
-        
+        <div className="footer">
+          <h2>用 ❤️ 打造 by frens at Logic Clock</h2>
+          <p>逻辑时钟提供了一种在分布式系统中确定事件顺序的方法，它不依赖于物理时钟，因此即使在服务器时钟出现问题时，也能保持事件的因果关系和顺序。</p>
+          <div>© Lethe </div>
+          <div>© Deng </div>
+        </div>
       </div>
     </>
   )
